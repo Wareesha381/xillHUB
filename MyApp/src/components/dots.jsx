ce2ff343e68784bf4f81fbc77bdc4dba9b23d9c7
Wareesha381 <wareeshamalik034.gmail.com>
2026-09-08 13:52:00 +0100

    Update Background Sparkle

--- MyApp/src/components/dots.jsx
+++ MyApp/src/components/dots.jsx
@@ -3,19 +3,21 @@ import { useEffect, useState } from "react";
 const Signs = ["*", "&", "{}", "[]", "</>", "#", "=="];
 
 function SparkleField() {
-  let [sign, SetElement] = useState([]);
+  let [sign, SetElement] = useState([[], [], []]);
   const [sparcle, setSparkel] = useState(false);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      let top = Math.random() * 100;
-      let left = Math.random() * 100;
+      let top = [];
+      let left = [];
+      let positions = [];
 
-      let element1 = Signs[Math.floor(Math.random() * Signs.length)];
-      let element2 = Signs[Math.floor(Math.random() * Signs.length)];
-      let element3 = Signs[Math.floor(Math.random() * Signs.length)];
-
-      SetElement([top, left, element1, element2, element3]);
+      for (let i = 0; i < 3; i++) {
+        positions[i] = Signs[Math.floor(Math.random() * Signs.length)];
+        left[i] = Math.random() * 100;
+        top[i] = Math.random() * 100;
+      }
+      SetElement([top, left, positions]);
       setTimeout(() => {
         setSparkel(true);
       }, 50);
@@ -26,33 +28,18 @@ function SparkleField() {
 
   return (
     <div className="sparkle-field">
-      <span
-        className={`${sparcle ? "sparkle-visible sparkle" : "sparkle"}`}
-        style={{
-          top: `${sign[0]}%`,
-          left: `${sign[1]}%`,
-        }}
-      >
-        {sign[2]}
-      </span>
-      <span
-        className={`${sparcle ? "sparkle-visible sparkle" : "sparkle"}`}
-        style={{
-          top: `${sign[0] + Math.random() * 100}%`,
-          left: `${sign[1] + Math.random() * 50}%`,
-        }}
-      >
-        {sign[3]}
-      </span>
-      <span
-        className={`${sparcle ? "sparkle-visible sparkle" : "sparkle"}`}
-        style={{
-          top: `${sign[0] + Math.random() * 100}%`,
-          left: `${sign[1] + Math.random() * 50}%`,
-        }}
-      >
-        {sign[4]}
-      </span>
+      {sign[2].map((element, i) => (
+        <span
+          key={i}
+          className={`${sparcle ? "sparkle-visible sparkle" : "sparkle"}`}
+          style={{
+            top: `${sign[0][i]}%`,
+            left: `${sign[1][i]}%`,
+          }}
+        >
+          {element}
+        </span>
+      ))}
     </div>
   );
 }
